f62f9b6312b8eeed1972676bfb54e9933644808c
Sam Harwell <sam@tunnelvisionlabs.com>
2018-08-30 06:35:58 -0500

    Remove the DefaultVerifier implementations of analyzer and code fix verifiers
    These types were showing up in the Add Using light bulb for code attempting to
    reference AnalyzerVerifier<TAnalyzer> from one of the test framework specific
    libraries (MSTest/NUnit/XUnit). These types are rarely used and trivial to
    implement by a user if necessary, so this pull request removes them to improve
    usability in majority scenarios.

--- src/Tools/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.Analyzer.Testing/AnalyzerVerifier.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports Microsoft.CodeAnalysis.Diagnostics
-Imports Microsoft.CodeAnalysis.Testing
-
-Namespace Microsoft.CodeAnalysis.VisualBasic.Testing.Default
-    Public Class AnalyzerVerifier(Of TAnalyzer As {DiagnosticAnalyzer, New})
-        Inherits VisualBasicAnalyzerVerifier(Of TAnalyzer, DefaultVerifier)
-    End Class
-End Namespace
--- src/Tools/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.VisualBasic.CodeFix.Testing/CodeFixVerifier.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports Microsoft.CodeAnalysis.CodeFixes
-Imports Microsoft.CodeAnalysis.Diagnostics
-Imports Microsoft.CodeAnalysis.Testing
-
-Namespace Microsoft.CodeAnalysis.VisualBasic.Testing.Default
-    Public Class CodeFixVerifier(Of TAnalyzer As {DiagnosticAnalyzer, New}, TCodeFix As {CodeFixProvider, New})
-        Inherits VisualBasicCodeFixVerifier(Of TAnalyzer, TCodeFix, DefaultVerifier)
-    End Class
-End Namespace
