53e47783a342f86fc576cca1cfae8e8db28466ba
Sam Harwell <sam@tunnelvisionlabs.com>
2018-09-11 00:19:48 -0500

    Rename VerifyExclusions to Exclusions and make it an enum

---
+++ src/Tools/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/AnalysisExclusions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.CodeAnalysis.Testing
+{
+    /// <summary>
+    /// Specifies characteristics of a file which cause diagnostics to not be reported.
+    /// </summary>
+    [Flags]
+    public enum AnalysisExclusions
+    {
+        /// <summary>
+        /// No special exclusions apply.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Analysis will not report diagnostics in generated code.
+        /// </summary>
+        GeneratedCode = 0x01,
+    }
+}
