f3a533f3aad2539d11edd3cfd6102ab23bc5d416
Chris Sienkiewicz <chsienki@microsoft.com>
2020-04-10 09:24:45 -0700

    Add initial source generator samples

---
+++ samples/CSharp/SourceGenerators/SourceGeneratorSamples/SettingsXmlGenerator.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Analyzer1
+{
+    [Generator]
+    public class SettingsXmlGenerator : ISourceGenerator
+    {
+        private const string SettingsFileString = @"
+namespace XmlSettings
+{
+    public partial class XmlSettings
+    {
+        
+    }
+}
+";
+
+        public object PathxmlFile { get; private set; }
+
+        public void Execute(SourceGeneratorContext context)
+        {
+            IEnumerable<AdditionalText> settingsFiles = context.AdditionalFiles.Where(at => at.Path.EndsWith(".xmlsettings"));
+            foreach (AdditionalText settingsFile in settingsFiles)
+            {
+                ProcessSettingsFile(settingsFile, context);
+            }
+        }
+        
+        private void ProcessSettingsFile(AdditionalText xmlFile, SourceGeneratorContext context)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            string text = xmlFile.GetText(context.CancellationToken).ToString();
+            try
+            {
+                xmlDoc.LoadXml(text);
+            }
+            catch
+            {
+                //TODO: issue a diagnostic that says we couldn't parse it
+                return;
+            }
+
+            string fileName = Path.GetFileName(xmlFile.Path);
+            string name = xmlDoc.DocumentElement.GetAttribute("name");
+
+            StringBuilder sb = new StringBuilder($@"
+namespace AutoSettings
+{{
+    using System;
+    using System.Xml;
+
+    public partial class XmlSettings
+    {{
+        
+        public static {name}Settings {name} {{ get; }} = new {name}Settings(""{fileName}"");
+
+        public class {name}Settings 
+        {{
+            
+            XmlDocument xmlDoc = new XmlDocument();
+                
+            internal {name}Settings(string fileName)
+            {{
+                xmlDoc.Load(fileName);
+            }}
+");
+
+            for(int i = 0; i < xmlDoc.DocumentElement.ChildNodes.Count; i++)
+            {
+                XmlElement setting = (XmlElement)xmlDoc.DocumentElement.ChildNodes[i];
+                string settingName = setting.GetAttribute("name");
+                string settingType = setting.GetAttribute("type");
+                string defaultValue = setting.GetAttribute("defaultValue");
+
+                sb.Append($@"
+
+public {settingType} {settingName}
+{{
+    get
+    {{
+        return ({settingType}) Convert.ChangeType(((XmlElement)xmlDoc.DocumentElement.ChildNodes[{i}]).InnerText, typeof({settingType}));
+    }}
+}}
+");
+            }
+
+            sb.Append("} } }");
+
+            context.AddSource($"Settings_{name}", SourceText.From(sb.ToString(), Encoding.UTF8));
+        }
+     
+        public void Initialize(InitializationContext context)
+        {
+        }
+
+        public partial class XmlSettings
+        {
+            private XmlDocument xmlDoc = new XmlDocument();
+
+            private XmlSettings(string fileName)
+            {
+                xmlDoc.Load(fileName);
+            }
+
+
+        }
+    }
+}
