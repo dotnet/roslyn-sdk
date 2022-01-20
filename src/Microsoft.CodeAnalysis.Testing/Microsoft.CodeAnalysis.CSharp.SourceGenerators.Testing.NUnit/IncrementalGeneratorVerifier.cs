32d3742d34587775ace6448e02b072c374760186
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2022-01-20 13:52:18 -0500

    Add support for IIncrementalGenerator

---
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.CSharp.SourceGenerators.Testing.NUnit/IncrementalGeneratorVerifier.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.CodeAnalysis.CSharp.Testing.NUnit
+{
+    public static class IncrementalGeneratorVerifier
+    {
+        public static IncrementalGeneratorVerifier<TIncrementalGenerator> Create<TIncrementalGenerator>()
+            where TIncrementalGenerator : IIncrementalGenerator, new()
+        {
+            return new IncrementalGeneratorVerifier<TIncrementalGenerator>();
+        }
+    }
+}
