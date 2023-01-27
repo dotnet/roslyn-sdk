a6a8b9adc3a9a75c9933d7606aec6d07aa8f68f1
Youssef Victor <youssefvictor00@gmail.com>
2023-01-27 14:21:40 +0200

    Update .NET 7 reference assemblies to latest stable

--- src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/ReferenceAssemblies.cs
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.Analyzer.Testing/ReferenceAssemblies.cs
@@ -987,7 +987,7 @@ namespace Microsoft.CodeAnalysis.Testing
                         "net7.0",
                         new PackageIdentity(
                             "Microsoft.NETCore.App.Ref",
-                            "7.0.0-preview.7.22375.6"),
+                            "7.0.2"),
                         Path.Combine("ref", "net7.0"));
                 });
 
