8f2dde016b91986d5d82cd0f33612942f8faf7dc
Aleksandr Sobolev <v-asobolev@microsoft.com>
2025-09-17 16:12:37 +0400

    Add net7 APIs

---
+++ src/Microsoft.CodeAnalysis.Testing/Microsoft.CodeAnalysis.CSharp.Analyzer.Testing/CSharpAnalyzerTest`2.net7.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+
+namespace Microsoft.CodeAnalysis.CSharp.Testing;
+
+public partial class CSharpAnalyzerTest<TAnalyzer, TVerifier>
+{
+    public static CSharpAnalyzerTest<TAnalyzer, TVerifier> Create([StringSyntax("C#-test")] string source, params DiagnosticResult[] expectedDiagnostics)
+    {
+        var test = new CSharpAnalyzerTest<TAnalyzer, TVerifier> { TestCode = source };
+        test.ExpectedDiagnostics.AddRange(expectedDiagnostics);
+
+        return test;
+    }
+}
